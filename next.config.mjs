25aa4eb72e7e710e00b2b2ec59b850464ec702c0
andres yair carvajal bolivar <ancarvajalb@unal.edu.co>
2025-05-13 16:30:00 -0500

    next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
+// next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   eslint: {
     ignoreDuringBuilds: true,
   },
@@ -7,14 +9,13 @@ const nextConfig = {
     ignoreBuildErrors: true,
   },
   images: {
-    unoptimized: true,
+    unoptimized: true,  // Desactiva optimización si usas imágenes locales
+    domains: ['localhost'],  // Permite imágenes de tu Strapi local
   },
+  // Opcional: Configuración para subir a Vercel
+  experimental: {
+    outputFileTracingRoot: path.join(__dirname, '../../'),
+  }
 }
 
-export default nextConfig
-
-
-/** @type {import('next').NextConfig} */
-module.exports = {
-  output: 'standalone', // Para menor tamaño de despliegue
-}
+export default nextConfig;
